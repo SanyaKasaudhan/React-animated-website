469632548f427ad515f24cb5d3c76c7912cd91c9
SanyaKasaudhan <kasaudhansanya@gmail.com>
2020-08-15 23:07:40 +0530

    service

--- src/Common.js
+++ src/Common.js
@@ -5,7 +5,7 @@ const Common =(props) =>{
     return(
         <>
         <section id="header" className="d-flex align-items-center">
-        <div className="container-fluid nav bg">
+        <div className="container-fluid">
         <div className="row">
             <div className="col-10 mx-auto">
                 <div className="row">
