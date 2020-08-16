7ea657c9ef6e842403b4d7d185f680616856ada3
SanyaKasaudhan <kasaudhansanya@gmail.com>
2020-08-16 11:20:40 +0530

    f

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 const Contact =() =>{
     return(
         <>
-        
-        <h1>Welcome contact</h1>
+        <div className="my-5">
+            <h1 className="text-center">Contact Us</h1>
+        </div>
+        <div className="container contact_div">
+            <div className="row">
+                <div className="col-md-6 col-10 mx-auto">
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
